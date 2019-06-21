c50447faf62e8f80845b1258bf0ed972a64b9d53
Ostroveanu Madalin <33639375+OstroveanuMadalin@users.noreply.github.com>
2019-06-21 08:23:13 -0700

    Add files via upload

--- Licenta/Licenta.ino
+++ Licenta/Licenta.ino
@@ -1,5 +1,6 @@
-#include <LiquidCrystal.h> // includes the LiquidCrystal Library 
+    #include <LiquidCrystal.h> // includes the LiquidCrystal Library 
     #include <Keypad.h>
+    #include <Adafruit_Fingerprint.h>
     #define buzzer 9
     #define trigPin 24
     #define echoPin 22
@@ -7,7 +8,7 @@
     #define greenLed 12
     long duration;
     int distance, initialDistance, currentDistance, i, loop_cnt=0, j;
-
+    int fingerID;
     int screenOffMsg =0; int screenOffMsg1 =0;
     String password="1234";
     String tempPassword;
@@ -22,7 +23,7 @@
     const byte ROWS = 4; //four rows
     const byte COLS = 4; //four columns
     char keypressed;
-    //void  enterPassword();
+    
     //define the cymbols on the buttons of the keypads
     char keyMap[ROWS][COLS] = {
       {'1','2','3','A'},
@@ -34,19 +35,43 @@
     byte colPins[COLS] = {18, 19, 20, 21}; //Column pinouts of the keypad
     Keypad myKeypad = Keypad( makeKeymap(keyMap), rowPins, colPins, ROWS, COLS); 
     LiquidCrystal lcd(8, 7 , 6, 5, 4, 3); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) 
+    SoftwareSerial mySerial(10, 11);
+    Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
     void setup() { 
-      lcd.begin(16,2); 
+      lcd.begin(16,2);     
       pinMode(redLed, OUTPUT);
       pinMode(greenLed, OUTPUT);
       pinMode(buzzer, OUTPUT); // Set buzzer as an output
       pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-      pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+      pinMode(echoPin, INPUT);
       
+      Serial.begin(9600);
+  
+      while (!Serial);  // For Yun/Leo/Micro/Zero/...
+        delay(100);
+        Serial.println("\n\nAdafruit finger detect test");
+
+        // set the data rate for the sensor serial port
+        finger.begin(57600);
+  
+         if (finger.verifyPassword()) {
+            Serial.println("Found fingerprint sensor!");
+            } else {
+                    Serial.println("Did not find fingerprint sensor :(");
+                    while (1) { delay(1); }
+                   }
+
+       finger.getTemplateCount();
+       Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+       Serial.println("Waiting for valid finger...");// Sets the echoPin as an Input
+            
     }
 
     void loop()
     {
       delay(100);
+      
           if (alarmActivated == true){
           currentDistance = getDistance(); 
           if ( currentDistance < initialDistance) {
@@ -57,6 +82,7 @@
             screenOffMsg=0;
           }
         }
+        
       if(!alarmActivated){
         noTone(buzzer);
         valid=false;
@@ -74,32 +100,18 @@
             if(keypressed== 'A'){
               lcd.clear();
               lcd.setCursor(0,0);
-              lcd.print("C-PAROLA");
+              lcd.print("C-Parola");
               lcd.setCursor(0,1);
-              lcd.print("D-AMPRENTA");
+              lcd.print("D-Amprenta");
             }
             if(keypressed=='C'){
               enterPassword();
               if(valid){  
                 loop_cnt=0;
-                activareAlarma(10);
-                //lcd.clear();
-                //lcd.setCursor(0,0);//activare alarma
-                //lcd.print("Se va activa alarma");
-               
-                
-                //led rosu aprins
-                //functie pentru activare
-                //
+                activareAlarma(9);
               }
-              //lcd.clear();
-              //lcd.setCursor(0,0);
-              //lcd.print("Introduceti Parola");
-            }else if(keypressed=='D'){
-              //functie pentru amprenta
-              lcd.clear();
-              lcd.setCursor(0,0);
-              lcd.print("Introduceti Amprenta");
+            }else if(keypressed=='D'){    
+              detectFingerPrint();
             }else if(keypressed=='#'){
               screenOffMsg=0;
             } else if (keypressed =='B') {
@@ -108,7 +120,7 @@
       tone(buzzer, 2000, 100);
       tempPassword = "";
       lcd.setCursor(0,0);
-      lcd.print("Current Password");
+      lcd.print("Parola curenta");
       lcd.setCursor(0,1);
       lcd.print(">");
       passChangeMode = true;
@@ -142,7 +154,7 @@
           tempPassword="";
           lcd.clear();
           lcd.setCursor(0,0);
-          lcd.print("Set New Password");
+          lcd.print("Parola noua");
           lcd.setCursor(0,1);
           lcd.print(">");
           while(passChangeMode) {
@@ -164,7 +176,7 @@
               tone(buzzer, 2000, 100);
               lcd.clear();
               lcd.setCursor(0,0);
-              lcd.print("Set New Password");
+              lcd.print("Parola noua");
               lcd.setCursor(0,1);
               lcd.print(">");
             }
@@ -181,12 +193,15 @@
       }
     }
    }
- 
+   }
+  
+   
+    
 
              
             
       
-      }else { 
+      else { 
         valid=false;
         digitalWrite(redLed,LOW);
         digitalWrite(greenLed,HIGH);
@@ -195,32 +210,36 @@
           lcd.setCursor(0,0);
           lcd.print("Alarma activata");
           screenOffMsg1=1;
-        }
+        } lcd.setCursor(0,1);
+          lcd.print("A-Dezactivare");
           keypressed= myKeypad.getKey();
           if(keypressed=='A'){
             lcd.clear();
             lcd.setCursor(0,0);
-            lcd.print("Dezactivare");
+            lcd.print("Dezactivare");       
             enterPassword();
-            if(valid){
+             if(valid){
               alarmActivated=false;
               screenOffMsg=0;
             }
             
           }
         
-    }
+      }
     
-}
+   }
+   
+    
+    
 void enterPassword() {
-      int k=5;
+      int k=7;
       tempPassword = "";
       activated = true;
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print(" *** ALARM *** ");
+      lcd.print("Introduceti");
       lcd.setCursor(0,1);
-      lcd.print("Pass>");
+      lcd.print("Parola>");
           while(activated) {
           keypressed = myKeypad.getKey();
           if (keypressed != NO_KEY){
@@ -233,14 +252,14 @@ void enterPassword() {
               k++;
             }
           }
-          if (k > 9 || keypressed == '#') {
+          if (k > 11 || keypressed == '#') {
             tempPassword = "";
             k=5;
             lcd.clear();
             lcd.setCursor(0,0);
-            lcd.print(" *** ALARM *** ");
+            lcd.print("Introduceti");
             lcd.setCursor(0,1);
-            lcd.print("Pass>");
+            lcd.print("Parola>");
           }
           if ( keypressed == '*') {
             if ( tempPassword == password ) {
@@ -249,69 +268,49 @@ void enterPassword() {
             }
             else if (tempPassword != password) {
               lcd.setCursor(0,1);
-              lcd.print("Wrong! Try Again");
+              lcd.print("Parola gresita");
               delay(2000);
               lcd.clear();
               lcd.setCursor(0,0);
-              lcd.print(" *** ALARM *** ");
+              lcd.print("Introduceti");
               lcd.setCursor(0,1);
-              lcd.print("Pass>");
+              lcd.print("Parola>");
               valid=false;
             }
           }    
         }
     }
    long getDistance(){
-      //int i=10;
-      
-      //while( i<=10 ) {
-      // Clears the trigPin
+     
       digitalWrite(trigPin, LOW);
       delayMicroseconds(2);
-      // Sets the trigPin on HIGH state for 10 micro seconds
+      
       digitalWrite(trigPin, HIGH);
       delayMicroseconds(10);
       digitalWrite(trigPin, LOW);
-      // Reads the echoPin, returns the sound wave travel time in microseconds
+      
       duration = pulseIn(echoPin, HIGH);
-      // Calculating the distance
+      
       distance = duration*0.034/2;
-      //sumDistance += distance;
-      //}
-      //int averageDistance= sumDistance/10;
+      
       return distance;
     }
-/* long getDistance(){
-    digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-  distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-    return distance;
- }*/
 
   void activareAlarma(int j){
-    while(j>=0){
-      delay(200);
-      loop_cnt++;
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("Alarma se");
       lcd.setCursor(0,1);
       lcd.print("activeaza in:");   
-      lcd.print(j);
-
-        
-        
-        if (loop_cnt==5){
+      
+      while(j>=0){
+       delay(50);
+       loop_cnt++;
+       lcd.setCursor(13,1);
+       lcd.print(j);
+        if (loop_cnt==20){
           loop_cnt=0;
-          j--;
+          j--;        
           tone(buzzer,2000);
         }  else 
           {
@@ -326,4 +325,104 @@ void enterPassword() {
         
       
   }
- 
+
+
+
+ uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+
+  return finger.fingerID;
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+}
+
+
+void detectFingerPrint(){
+   //finger.fingerID=0;
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print("Detectare");
+     lcd.setCursor(5,1);
+     lcd.print("amprenta");
+      while(!alarmActivated){
+        delay(50);
+        fingerID=getFingerprintIDez();
+        if (fingerID == 1 || fingerID==2 || fingerID==3)
+           activareAlarma(9);
+    }
+}
